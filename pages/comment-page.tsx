d1ff8e80d5cac64bdf0b5cf1944bfef8b0f89e21
Msksgm <msksgm3124@gmail.com>
2022-07-03 15:59:24 +0900

    add: 12. Testing of page navigation

---
+++ pages/comment-page.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+
+const CommentPage: React.FC = () => {
+  return (
+    <Layout title="Comment">
+      <p className="text-4xl m-10">comment page</p>
+    </Layout>
+  )
+}
+export default CommentPage
